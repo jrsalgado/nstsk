3c4a6dedd0dbb3e36830290ec60ac02c902e6a31
Joshua Saucedo <joshuansu0897@gmail.com>
2020-07-07 19:27:49 -0700

    using network module in cloud platform

--- platform/cloud/main.tf
+++ platform/cloud/main.tf
@@ -6,6 +6,8 @@ provider "aws" {
 
 module "network" {
   source = "../../modules/network"
-  // names= []
-  // regiion = []
+
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
 }
