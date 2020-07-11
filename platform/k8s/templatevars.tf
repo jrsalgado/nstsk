ffc02363759f449aaad2453dcb3792297496d52c
Joshua Saucedo <joshuansu0897@gmail.com>
2020-07-11 16:56:31 -0700

    add platform k8s

---
+++ platform/k8s/templatevars.tf
@@ -0,0 +1,3 @@
+variable "app_env" {
+    default = "dev"
+}
---
+++ platform/k8s/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_profile" {
+  description = "AWS Profile"
+}
+
+# Network
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDRs for the public subnets"
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDRs for the private subnets"
+}
