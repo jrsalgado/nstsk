a25f016acfaa4d3420dd6a6da341c53ae686fd90
Axel Monroy <>
2020-09-08 16:14:11 -0700

    add missing vars

--- platform/stateful-application/main.tf
+++ platform/stateful-application/main.tf
@@ -5,14 +5,18 @@ provider "aws" {
 }
 
 
+data "http" "my_ip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
 module "ami" {
   source = "../../modules/ami"
 }
 
 module "wordpress" {
-  source          = "../../modules/wordpress"
-  ami             = module.ami_ubuntu_18_04
-  app_env         = var.app_env
-  private_subnets = data.terraform_remote_state.cloud.outputs.private_subnets.*.id
-
+  source               = "../../modules/wordpress"
+  ami                  = module.ami_ubuntu_18_04
+  app_env              = var.app_env
+  allow_ssh_cidr_block = "${chomp(data.http.my_ip.body)}/32"
+  private_subnets      = data.terraform_remote_state.cloud.outputs.private_subnets.*.id
 }
