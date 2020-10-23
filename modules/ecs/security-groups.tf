a8cf8eb725d9d09f3eafa9dc5f245eee2aaea758
Alfredo Cambera <alfredo.cambera@encora.com>
2020-10-23 16:18:42 -0500

    fixed sg for auto-scaling group

--- modules/ecs/security-groups.tf
+++ modules/ecs/security-groups.tf
@@ -3,20 +3,11 @@ resource "aws_security_group" "ecs_instances_sg" {
   description = "ECS instances security group"
   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = [
-    "0.0.0.0/0"]
-  }
-
   # allow incomming connections to ephemeral ports
   ingress {
     from_port       = 1024
     to_port         = 65535
     protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
     security_groups = [aws_security_group.alb_sg.id]
   }
 
