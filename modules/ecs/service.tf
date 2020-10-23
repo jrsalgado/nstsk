3bdf0ef1da927a14966bf9ac405e24ee389561a0
Alfredo Cambera <alfredo.cambera@encora.com>
2020-10-23 18:13:04 -0500

    workaround - reduced the # of task to 1 to make the app work

--- modules/ecs/service.tf
+++ modules/ecs/service.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "ecs_service" {
   iam_role        = aws_iam_role.ecs_service_role.name
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = "${aws_ecs_task_definition.wordpress.family}:${max("${aws_ecs_task_definition.wordpress.revision}", "${data.aws_ecs_task_definition.wordpress.revision}")}"
-  desired_count   = 2
+  desired_count   = 1
   load_balancer {
     target_group_arn = aws_alb_target_group.ecs_target_group.arn
     container_port   = 80
