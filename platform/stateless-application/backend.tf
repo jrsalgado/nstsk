9cd054dcd604f1d0a36d8e499d91ee10664038e7
Alfredo Cambera <alfredo.cambera@encora.com>
2020-12-05 21:11:53 -0600

    removed unnecessary comment

--- platform/stateless-application/backend.tf
+++ platform/stateless-application/backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "s3" {
-    # From Makefile
-    # terraform init  -backend-config="bucket=nstask" -backend-config="key=Task/develop"
-    # key    = "nstask/Matillion"
     region         = "us-east-1"
     dynamodb_table = "terraform_states"
   }
